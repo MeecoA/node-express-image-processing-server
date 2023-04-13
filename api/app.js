3cc2d68d5367c457c1232769efa8fe0862fd4841
Coa <meecorcll@gmail.com>
2023-04-13 08:52:06 +0800

    init

--- api/app.js
+++ api/app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
-const pathToIndex = path.resolve(__dirname, "..client/index.html");
-
-// app.use("/*", (request, response) => {
-//   response.sendFile(pathToIndex);
-// });
+const pathToIndex = path.resolve(__dirname, "../client/index.html");
 
 app.use("/*", (request, response) => {
   response.sendFile(pathToIndex);
