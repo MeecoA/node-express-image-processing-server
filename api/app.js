82361ec301d45366956f347a4939b78d401fc473
Coa <meecorcll@gmail.com>
2023-04-13 08:51:14 +0800

    init

--- api/app.js
+++ api/app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const pathToIndex = path.resolve(__dirname, "..client/index.html");
 
+// app.use("/*", (request, response) => {
+//   response.sendFile(pathToIndex);
+// });
+
 app.use("/*", (request, response) => {
   response.sendFile(pathToIndex);
 });
